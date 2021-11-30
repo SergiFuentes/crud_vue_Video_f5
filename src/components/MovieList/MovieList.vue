b8a5af64ce641487fbdd2680bf698b0d28f12c81
SergiFuentes <you@example.com>
2021-11-30 11:23:58 +0100

    adding MovieList component

---
+++ src/components/MovieList/MovieList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <ul>
+            <li v-for="movie in movies">Título</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            msg:"Lista de Pelis",
+            movies: [1,2,3,4]
+
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+ul{
+    list-style: none;
+}
+</style>
+   
